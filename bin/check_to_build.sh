4d6b8834d8b09dd254b3024c2fbd3ecb1944bcf7
Isaiah Mann <isaiah@truex.com>
2019-01-15 21:52:33 -0800

    Call Travis terminate from YAML file

--- bin/check_to_build.sh
+++ bin/check_to_build.sh
@@ -2,13 +2,11 @@
 response_code=$(curl -s -o  /dev/null -w "%{http_code}" "https://api.github.com/repos/imann24/travis-tests/releases/tags/${TRAVIS_TAG}")
 if [[ $response_code -ne "200" ]]; then
     echo "Tag does not have an associated release. Skipping build"
-    travis_terminate 0
-    exit 0
+    exit 1
 fi
 master_branch_info=$(curl https://api.github.com/repos/imann24/travis-tests/git/refs/heads/master)
 latest_master_commit=$(python bin/get_sha.py $master_branch_info)
 if [[ $latest_master_commit != $TRAVIS_COMMIT ]]; then
     echo "Commit ${TRAVIS_COMMIT} is not the latest change in master. Skipping build"
-    travis_terminate 0
-    exit 0
+    exit 2
 fi
