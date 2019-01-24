d983c043ea21a9fc691b72b659f9e654cb9bbd75
Isaiah Mann <isaiah@truex.com>
2019-01-24 10:18:36 -0800

    Cleanup build check script

--- bin/check_to_build.sh
+++ bin/check_to_build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
-response_code=$(curl -s -o  /dev/null -w "%{http_code}" "https://api.github.com/repos/imann24/travis-tests/releases/tags/${TRAVIS_TAG}")
+owner=$1
+repository=$2
+response_code=$(curl -s -o  /dev/null -w "%{http_code}" "https://api.github.com/repos/${owner}/{$repository}/releases/tags/${TRAVIS_TAG}")
 if [[ $response_code -ne "200" ]]; then
     echo "Tag does not have an associated release. Skipping build"
     exit 1
 fi
-master_branch_info=$(curl https://api.github.com/repos/imann24/travis-tests/git/refs/heads/master)
-latest_master_commit=$(python bin/get_sha.py $master_branch_info)
+master_branch_info=$(curl https://api.github.com/repos/${owner}/${repository}/git/refs/heads/master)
+latest_master_commit=$(python bin/get_sha.py "$master_branch_info")
 if [[ $latest_master_commit != $TRAVIS_COMMIT ]]; then
     echo "Commit ${TRAVIS_COMMIT} is not the latest change in master. Skipping build"
     exit 2
--- bin/get_sha.py
+++ bin/get_sha.py
@@ -1,14 +1,11 @@
 import sys
 import json
 
-next_arg_is_commit = False
-for arg in sys.argv:
-    if next_arg_is_commit:
-        commit_hash = arg
-        commit_hash = commit_hash.replace("\"", "")
-        commit_hash = commit_hash.replace(",", "")
-        print commit_hash
-        sys.exit(0)
-    next_arg_is_commit = "\"sha\":" == arg
-print "No commit hash found"
-sys.exit(1)
+if len(sys.argv) < 2:
+    print "No ref payload provided"
+    sys.exit(1)
+sha = json.loads(sys.argv[1])["object"]["sha"]
+if not sha:
+    print "No commit hash found"
+    sys.exit(2)
+print sha
