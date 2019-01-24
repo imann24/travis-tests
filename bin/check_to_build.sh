cb04a28d5dd229eba6a324056db49bf3e1285ab6
Isaiah Mann <isaiah@truex.com>
2019-01-24 11:20:21 -0800

    Fix typo in script

--- bin/check_to_build.sh
+++ bin/check_to_build.sh
@@ -6,7 +6,7 @@ if [[ -z "$TRAVIS_TAG" ]]; then
     echo "Commit has no tag. Skipping build"
     exit 1
 fi
-response_code=$(curl -s -o  /dev/null -w "%{http_code}" "https://api.github.com/repos/${owner}/{$repository}/releases/tags/${TRAVIS_TAG}")
+response_code=$(curl -s -o  /dev/null -w "%{http_code}" "https://api.github.com/repos/${owner}/${repository}/releases/tags/${TRAVIS_TAG}")
 if [[ $response_code -ne "200" ]]; then
     echo "Tag does not have an associated release. Skipping build"
     exit 2
